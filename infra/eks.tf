f094282aa661349444f8fd414647c0a0525a9c22
Diego <dieg.sandovalp@duocuc.cl>
2026-06-29 18:49:22 -0400

    da

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,10 +1,7 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
-
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-
+  
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
