2de7906f98396eb8c2be4d98156e05d97c01fa55
Diego <dieg.sandovalp@duocuc.cl>
2026-06-29 18:51:29 -0400

    da

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,9 +1,17 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
   
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   iam_role_arn = "arn:aws:iam::446959396764:role/LabRole"
+
+
+  create_iam_role      = false
+  enable_irsa          = false
+  create_kms_key       = false
+  cluster_encryption_config = {}
+
+
+  create_cloudwatch_log_group = false
 }
