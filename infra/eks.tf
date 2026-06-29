be6c10b9bdc2c6fc6e5c0f8c669ffaa0bf4a3251
Diego <dieg.sandovalp@duocuc.cl>
2026-06-29 18:42:58 -0400

    a

--- infra/eks.tf
+++ infra/eks.tf
@@ -4,4 +4,14 @@ module "eks" {
 
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  enable_irsa = true
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
