b75e99e01d980dabea9d436dfda086d0b00ea2e0
Diego <dieg.sandovalp@duocuc.cl>
2026-06-29 18:48:40 -0400

    adas

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,4 +1,12 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.24.0" # Fija la versión explícitamente
+  version = "~> 21.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  iam_role_arn = "arn:aws:iam::446959396764:role/LabRole"
 }
