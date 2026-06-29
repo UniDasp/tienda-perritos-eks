cfcc5a38d5b3d62fa1599ef56def0e07c464c4ac
Diego <dieg.sandovalp@duocuc.cl>
2026-06-29 18:45:10 -0400

    a

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,17 +1,4 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
-
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-
-  vpc_id     = var.vpc_id
-  subnet_ids = var.subnet_ids
-
-  enable_irsa = true
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
+  version = "21.24.0" # Fija la versión explícitamente
 }
