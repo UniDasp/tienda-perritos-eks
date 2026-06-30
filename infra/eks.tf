1e58dd93468af3073c68f19c220a5a485a8ab54c
Diego <dieg.sandovalp@duocuc.cl>
2026-06-29 21:02:42 -0400

    Update eks.tf

--- infra/eks.tf
+++ infra/eks.tf
@@ -5,13 +5,22 @@ resource "aws_eks_cluster" "main" {
   vpc_config {
     subnet_ids = module.vpc.private_subnets
   }
+
+  enabled_cluster_log_types = [
+    "api",
+    "audit",
+    "authenticator",
+    "controllerManager",
+    "scheduler"
+  ]
 }
 
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "nodes"
   node_role_arn   = "arn:aws:iam::446959396764:role/LabRole"
-  subnet_ids      = module.vpc.private_subnets
+
+  subnet_ids = module.vpc.private_subnets
 
   scaling_config {
     desired_size = 1
